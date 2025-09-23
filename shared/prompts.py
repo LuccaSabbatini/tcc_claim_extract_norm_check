86bffce8518a572952661be877274c140552b3cd
LuccaSabbatini <sabbatini.lucca@gmail.com>
2025-09-23 20:47:34 -0300

    initial commit

---
+++ shared/prompts.py
@@ -0,0 +1,11 @@
+# CLEF CheckThat! Lab 2025 Task 2 Rank 2 Prompt
+CLAIM_NORMALIZATION_RANK_2 = """You have received an informal and disorganized social media post. Summarize this post into a clear and concise
+statement, without adding any new information.
+Post: {original_post}
+Normalized statement:"""
+
+# CLEF CheckThat! Lab 2025 Task 2 Rank 3 Prompt
+CLAIM_NORMALIZATION_RANK_3 = """You have received an informal and disorganized social media post. Summarize this post into a clear and concise
+statement, without adding any new information.
+Post: {original_post}
+Normalized statement:"""
