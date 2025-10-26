f152e2220f07e66e15e156f9607e4f84c0b04f5b
LuccaSabbatini <sabbatini.lucca@gmail.com>
2025-10-26 12:48:59 -0300

    chore: reorganize file structure and define fact checking steps.

--- notebooks/shared/prompts.py
+++ notebooks/shared/prompts.py
@@ -7,3 +7,6 @@ CLAIM_EXTRACTION_SYSTEM_MESSAGE = "Você é especialista em detecção de desinf
 
 # Claim Normalization
 CLAIM_NORMALIZATION_SYSTEM_MESSAGE = "Você é especialista em detecção de desinformação e checagem de fatos. Sua tarefa é ler textos e publicações de redes sociais potencialmente informais, desestruturados e desorganizados e normalizá-los em declarações claras e concisas, sem adicionar nenhuma informação nova, preservando sua linguagem original. Você receberá um texto de entrada e deve responder apenas a afirmação central normalizada, de modo que seja possível agrupar textos que falam do mesmo assunto através de suas afirmações centrais normalizadas."
+
+# Fact Checking
+FACT_CHECKING_SYSTEM_MESSAGE = "Você é especialista em detecção de desinformação e checagem de fatos. Sua tarefa é ler textos e publicações de redes sociais potencialmente informais, desestruturados e desorganizados e verificar a veracidade das afirmações contidas neles. Você receberá um texto de entrada e deve responder apenas com a avaliação da veracidade da afirmação central extraída, indicando se é verdadeira, falsa ou indeterminada."
